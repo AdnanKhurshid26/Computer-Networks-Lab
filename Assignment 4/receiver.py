d7ded47064736f90d7fd52e18a60b0129c001e6a
Adnan Khurshid <adnankhurshid251@gmail.com>
2022-10-10 22:20:29 +0530

    Assignment 4 finsished

--- Assignment 4/receiver.py
+++ Assignment 4/receiver.py
@@ -3,23 +3,22 @@ import socket
 import json
 
 sock = socket.socket()
-sock.connect(('127.0.0.1',8080))
+sock.connect(('127.0.0.1', 8080))
 
+#receiving the number of stations from the sender
 n = int(sock.recv(1024).decode())
 
 N = getNextPowerof2(n)
 
-print("Generating walsh table...")
-
 walshtable = None
 
-if(N==1):
+if (N == 1):
     walshtable = [[1]]
 else:
     walshtable = [[0 for i in range(N)] for j in range(N)]
     buildWalshTable(walshtable, N, 0, N - 1, 0, N - 1, False)
 
-print("Walsh Table Generated")
+print("\nWalsh Table Generated")
 
 for i in walshtable:
     print(i)
@@ -27,31 +26,30 @@ for i in walshtable:
 print("\n")
 
 data = sock.recv(4096)
-res = json.loads(data.decode())
+data = json.loads(data.decode())
 
 
-decoded_words = []
+decoded = []
 
+#initilaizing the list of messages with n empty strings
 for i in range(n):
-    decoded_words.append("")
-    
-for i in range(len(res)):
+    decoded.append("")
+
+for i in range(len(data)):
     for j in range(n):
-        sum =0
+        sum = 0
         for k in range(N):
-           sum += res[i][k]*walshtable[j][k]
+            sum += data[i][k]*walshtable[j][k]
         x = sum//N
-        
+
         if x == 1:
-            decoded_words[j] += "1"
+            decoded[j] += "1"
         elif x == -1:
-            decoded_words[j] += "0"
+            decoded[j] += "0"
         else:
+            #if we get 0 then it means the station is silent
             pass
-        
+
 
 for i in range(n):
-    print(f"Station {i+1}:-  {BinaryToStr(decoded_words[i])}")
-    
-
-    
+    print(f"Station {i+1}:-  {BinaryToStr(decoded[i])}")
--- Assignment 4/sender.py
+++ Assignment 4/sender.py
@@ -2,7 +2,6 @@ from helper import *
 import socket
 import json
 
-
 sock = socket.socket()
 sock.bind(('127.0.0.1', 8080))
 sock.listen(1)
@@ -13,36 +12,42 @@ n = int(input("Enter the number of stations:  "))
 
 N = getNextPowerof2(n)
 
+#sending the number of stations to the receiver first of all
 conn.send(str(n).encode())
 
-print("Generating walsh table...")
 
 walshtable = None
 
 if (n == 1):
     walshtable = [[1]]
 else:
+    #make an nxn zero matrix
     walshtable = [[0 for i in range(N)] for j in range(N)]
+    
     buildWalshTable(walshtable, N, 0, N - 1, 0, N - 1, False)
 
-print("Walsh Table Generated")
+print("\nWalsh Table Generated")
 
 for i in walshtable:
     print(i)
 
+print("\n")
+
+#to store the binary string of the message of each station in the form of a list
 binarystrings = []
 
 for i in range(n):
     data = input(f"\nEnter the data for the Station {i + 1}:-  ")
     binarystrings.append([*strTobinary(data)])
 
-print("\nBinary Words:-  ", binarystrings)
 
-
+print("\n")
+print("Encoded Data:-  \n")
 count = 0
 datatosend = []
 while count < n:
     encoded = []
+    #initilaizing the enncoded message of N length with 0
     for i in range(N):
         encoded.append(0)
     for i in range(len(binarystrings)):
@@ -55,9 +60,10 @@ while count < n:
                     encoded[j] += walshtable[i][j]*(-1)
 
             binarystrings[i].pop(0)
+            #if a binary string is eexhausted it means one message has been encoded fully
             if (len(binarystrings[i]) == 0):
                 count += 1
-
+    print(encoded)
     datatosend.append(encoded)
 
 res = json.dumps(datatosend)
